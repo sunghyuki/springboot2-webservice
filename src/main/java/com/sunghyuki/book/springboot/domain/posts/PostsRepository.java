70dad20f3fb668575f5261cbc1e73d103cc227d9
sunghyuki <sunghyuk1609@gmail.com>
2021-10-27 11:30:47 +0900

    feat: Spring Data Jpa 적용하기 및 테스트 코드 작성하기

---
+++ src/main/java/com/sunghyuki/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.sunghyuki.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}

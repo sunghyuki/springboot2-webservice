7281324a2dd1c47df4c0b5d5fc67b9e5e2969644
sunghyuki <sunghyuk1609@gmail.com>
2021-10-30 00:10:13 +0900

    feat: 머스테치로 화면 구성하기

--- src/main/java/com/sunghyuki/book/springboot/service/PostsService.java
+++ src/main/java/com/sunghyuki/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.sunghyuki.book.springboot.service;
 
 import com.sunghyuki.book.springboot.domain.posts.Posts;
 import com.sunghyuki.book.springboot.domain.posts.PostsRepository;
+import com.sunghyuki.book.springboot.web.dto.PostsListResponseDto;
 import com.sunghyuki.book.springboot.web.dto.PostsResponseDto;
 import com.sunghyuki.book.springboot.web.dto.PostsSaveRequestDto;
 import com.sunghyuki.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,8 +20,7 @@ public class PostsService {
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
-        return postsRepository.save(requestDto.toEntity())
-                .getId();
+        return postsRepository.save(requestDto.toEntity()).getId();
     }
 
     @Transactional
@@ -28,6 +31,21 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById(Long id) {
